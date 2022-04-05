9cfbdc8237593712db9c2e9fb5a1155dd1df1b1c
Andre Ferraz <andre.lopes@uaubox.com.br>
2022-04-05 16:58:43 -0300

    change script 2

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ function saveExcel(event){
   event.preventDefault();
 
   let name = document.getElementById("name").value;
-  console.log(name)
 
   let email = document.getElementById("email").value;
-  console.log(email)
 
   let url = "https://script.google.com/macros/s/AKfycbwJScFseAiwEOGUbDnN8AaqWHCiA7gmhjOey1l_xP0RcxujUB6x/exec?"
   
