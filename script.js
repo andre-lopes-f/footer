e49bb2d9bc14cde6f7db6922182001a4207f4018
Andre Ferraz <andre.lopes@uaubox.com.br>
2022-04-05 16:58:36 -0300

    change script

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 function saveExcel(event){
-  event.preventDefault()
+  event.preventDefault();
 
-  let name = document.getElementById("name");
+  let name = document.getElementById("name").value;
+  console.log(name)
 
-  let email = document.getElementById("email");
+  let email = document.getElementById("email").value;
+  console.log(email)
 
   let url = "https://script.google.com/macros/s/AKfycbwJScFseAiwEOGUbDnN8AaqWHCiA7gmhjOey1l_xP0RcxujUB6x/exec?"
   
-
+  console.log("Entrou");
   fetch(url + new URLSearchParams({
-  name,
-  email}), {method: 'GET'})
+    name: name.value,
+    email
+  }), {method: 'GET'})
     .then(response => {
         response.json()
             .then(data => {
                 console.log(data)
             })
     })
-
-
 }
